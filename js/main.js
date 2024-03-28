7c3f00b5b0dd706d7c93d1445a4828b1531bae62
Pavananvetri <147833700+Pavananvetri@users.noreply.github.com>
2024-03-29 01:45:26 +0530

    js update

--- js/main.js
+++ js/main.js
@@ -453,3 +453,101 @@ particlesJS("particles-js", {
 	},
 	"retina_detect": true
   });
+  let itemsImgs = document.querySelectorAll(".project-grid a img");
+  itemsImgsArr = Array.from(itemsImgs);
+  console.log(itemsImgsArr)
+  
+  // pop slider
+  let popSlider = document.querySelector(".resume-mf .modal-layer");
+  let bgSlide = document.querySelector(".modal-layer .slide-img");
+  
+  // icons/buttons
+  // close
+  let closeBtn = document.querySelector("#closeBtn");
+  closeBtn.style.cssText = `cursor : pointer;`;
+  // left
+  let prevBtn = document.querySelector("#prev");
+  prevBtn.style.cssText = `cursor : pointer;`;
+  // right
+  let nextBtn = document.querySelector("#next");
+  nextBtn.style.cssText = `cursor : pointer;`;
+  let subimages = [["img/funskool/Pavanan%20Portfolio-02.png", "img/funskool/Pavanan%20Portfolio-03.png", "img/funskool/Pavanan%20Portfolio-04.png"],
+  					["img/Flintobox/Pavanan%20Portfolio-05.png","img/Flintobox/Pavanan%20Portfolio-06.png", "img/Flintobox/Pavanan%20Portfolio-07.png", "img/Flintobox/Pavanan%20Portfolio-08.png"],
+  					["img/Vaya/Pavanan%20Portfolio-09.png","img/Vaya/Pavanan%20Portfolio-10.png","img/Vaya/Pavanan%20Portfolio-11.png","img/Vaya/Pavanan%20Portfolio-12.png","img/Vaya/Pavanan%20Portfolio-13.png","img/Vaya/Pavanan%20Portfolio-14.png","img/Vaya/Pavanan%20Portfolio-15.png",
+						"img/Vaya/Pavanan%20Portfolio-16.png","img/Vaya/Pavanan%20Portfolio-17.png", "img/Vaya/Pavanan%20Portfolio-18.png","img/Vaya/Pavanan%20Portfolio-19.png", "img/Vaya/Pavanan%20Portfolio-20.png", "img/Vaya/Pavanan%20Portfolio-21.png", "img/Vaya/Pavanan%20Portfolio-22.png",
+						"img/Vaya/Pavanan%20Portfolio-23.png","img/Vaya/Pavanan%20Portfolio-24.png", "img/Vaya/Pavanan%20Portfolio-25.png", "img/Vaya/Pavanan%20Portfolio-26.png","img/Vaya/Pavanan%20Portfolio-27.png",
+						"img/Vaya/Pavanan%20Portfolio-28.png","img/Vaya/Pavanan%20Portfolio-29.png"],
+					["img/Valeo/Pavanan%20Portfolio-33.png", "img/Valeo/Pavanan%20Portfolio-34.png", "img/Valeo/Pavanan%20Portfolio-35.png"],
+					["img/CLRI/Pavanan%20Portfolio-30.png","img/CLRI/Pavanan%20Portfolio-31.png","img/CLRI/Pavanan%20Portfolio-32.png"],
+					["img/Adaptive%20design/Pavanan%20Portfolio-36.png","img/Adaptive%20design/Pavanan%20Portfolio-37.png","img/Adaptive%20design/Pavanan%20Portfolio-38.png"],
+				
+			
+			];
+  // index of item
+  let urlString = "http://127.0.0.1:5500/pavananvetri.github.io/img/Adaptive%20design/Pavanan%20Portfolio-36.png";
+
+
+// splitUrl[0] will contain "http://127.0.0.1:5500/pavananvetri.github.io/"
+// To include "img/" in the first part, you can append it back
+
+  let activeIndex =0;
+  let indexoption=0;
+  let  imageSize = Array.from(subimages[activeIndex]);
+  for (let i = 0; i < itemsImgs.length; i++) {
+	itemsImgs[i].addEventListener("click", function (e) {
+	console.log(i)
+	  popSlider.style.display = "flex";
+	  
+	  let activeSrc = e.target.src;
+	  splitUrl = activeSrc.split("img/");
+	  activeSrc = splitUrl[0]+subimages[i][0];
+	  console.log(activeSrc);
+	  console.log(activeSrc);
+	  console.log(imageSize);
+	  imageSize = Array.from(subimages[i]);
+	  bgSlide.style.backgroundImage = `url(${activeSrc})`;
+	  activeIndex = itemsImgsArr.indexOf(e.target);
+	  console.log(activeIndex)
+	});
+  }
+  
+  closeBtn.addEventListener("click", removeSlider);
+  
+  function removeSlider() {
+	popSlider.style.display = "none";
+  }
+  
+  nextBtn.addEventListener("click", nextSlider);
+  
+  function nextSlider() {
+	indexoption++;
+
+	if (indexoption == imageSize.length) {
+	  indexoption = 0;
+	}
+	console.log(subimages[activeIndex][indexoption]);
+	let imgSrc = splitUrl[0]+subimages[activeIndex][indexoption];
+	bgSlide.style.backgroundImage = `url(${imgSrc})`;
+  }
+  
+  prevBtn.addEventListener("click", prevSlider);
+  
+  function prevSlider() {
+	indexoption--;
+	if (indexoption < 0) {
+	  indexoption = imageSize.length - 1;
+	}
+	let imgSrc = splitUrl[0]+subimages[activeIndex][indexoption];;
+	bgSlide.style.backgroundImage = `url(${imgSrc})`;
+  }
+  
+  // keyboard events
+  document.addEventListener("keydown", function (e) {
+	if (e.key == "Escape") {
+	  removeSlider();
+	} else if (e.key == "ArrowRight") {
+	  nextSlider();
+	} else if (e.key == "ArrowLeft") {
+	  prevSlider();
+	}
+  });
